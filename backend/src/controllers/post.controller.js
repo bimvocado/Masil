d85f51c20392d854e32d0f0e5e99229172ba6015
slwnt31 <20241005@sungshin.ac.kr>
2026-05-25 05:10:13 +0900

    fix: userId 하드 코딩 삭제

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -11,8 +11,7 @@ const createPost = async (req, res, next) => {
   try {
     const { content, imageUrl, stuffId } = req.body;
 
-    // const userId = req.user.user.userId;
-    const userId = 1;
+    const userId = req.user.user.userId;
 
     const reqDTO = new CreatePostReqDTO(
       content,
@@ -65,8 +64,7 @@ const updatePost = async (req, res, next) => {
     const { postId } = req.params;
     const { content, imageUrl } = req.body;
 
-    // const userId = req.user.user.userId;
-    const userId = 1;
+    const userId = req.user.user.userId;
 
     const reqDTO = new UpdatePostReqDTO(content, imageUrl);
 
@@ -89,8 +87,7 @@ const deletePost = async (req, res, next) => {
   try {
     const { postId } = req.params;
 
-    // const userId = req.user.user.userId;
-    const userId = 1;
+    const userId = req.user.user.userId;
 
     const deleteResult = await postService.deletePost(
       Number(postId),
